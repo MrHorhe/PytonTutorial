5e6565587bb4c8e356921c774753c32d1a9d5aaf
GeorgeMonda <george.monda@codexworks.com>
2020-05-12 14:41:13 +0300

    no message

---
+++ tests/test_unittest.py
@@ -0,0 +1,12 @@
+import inc_dec
+import unittest
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_incdement(self):
+        self.assertEqual(inc_dec.increment(3), 4)
+    
+    def test_decrement(self):
+        self.assertEqual(inc_dec.decrement(3), 2)
+
+if __name__ == '__name__':
+    unittest.main()
